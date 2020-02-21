4ac71af87236b8730a1b283d2227f3bbb6d8cdfc
DAM_1 <DAM_1@PB01>
2020-02-21 19:04:24 +0100

    atributos, getters y setters

--- src/main/java/liceosorolla/Usuario.java
+++ src/main/java/liceosorolla/Usuario.java
@@ -1,5 +1,36 @@
 package main.java.liceosorolla;
-
+import java.util.Date;
 public class Usuario {
+	
+	private String nombre;
+	private String apellidos;
+	private Date fechaNacimiento;
+	private int identificador;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	public int getIdentificador() {
+		return identificador;
+	}
+	public void setIdentificador(int identificador) {
+		this.identificador = identificador;
+	}
+	
 
 }
