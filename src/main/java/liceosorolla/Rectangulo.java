21ff0ad16d7544b8d535c3783cba4cc74cdae78e
DAM_1 <DAM_1@PB04>
2020-03-06 20:37:49 +0100

    bloque rectangulo

--- src/main/java/liceosorolla/Rectangulo.java
+++ src/main/java/liceosorolla/Rectangulo.java
@@ -1,20 +1,17 @@
 package main.java.liceosorolla;
 
 public class Rectangulo {
+	private int lado1 = 0;
+	private int lado2 = 0;
 	
-	private int lado1;
-	private int lado2;
-	
-
 	public Rectangulo(int lado1, int lado2) {
+		
 		this.lado1 = lado1;
 		this.lado2 = lado2;
+		
+		
 	}
-
-	public String toString() {
-		return "lado1 " + lado1 + ", lado2 " + lado2;
-	}
-
+	
 	public int getLado1() {
 		return lado1;
 	}
@@ -28,5 +25,34 @@ public class Rectangulo {
 		this.lado2 = lado2;
 	}
 	
+	
+	public int calculaPerimetro() {
+		int perimetro = 0;
+		perimetro = lado1 * 2 + lado2 * 2;
+		return perimetro; 
+	}
 
+	public int calculaArea() {
+		int area = 0;
+		area= lado1 * lado2;
+		return area;
+	}
+	
+	public int calculaDiagonal() {
+		int diagonal = 0;
+		diagonal = lado1 * lado1 + lado2 * lado2;
+		return diagonal;
+	}
+	
+	
+	public int calculaCircuncrita() {
+		int perimetro = 0;
+		int radio = 0;
+		
+		perimetro = lado1 * lado1 + lado2 * lado2;
+		radio = perimetro / 2;
+		return radio;
+	}
+	
 }
+
--- src/test/java/liceosorolla/RectanguloTest.java
+++ src/test/java/liceosorolla/RectanguloTest.java
@@ -1,5 +1,43 @@
 package test.java.liceosorolla;
 
-public class RectanguloTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
+import org.junit.Test;
+import main.java.liceosorolla.Rectangulo;
+
+public class RectanguloTest {
+	private Rectangulo rectangulo;
+	
+	@Before
+	public void before() {
+		rectangulo = new Rectangulo(6,6);
+		}
+	
+	
+	/*@Test
+	public void compruebaLados() {
+		assertEquals(60,rectangulo.calculaLados());
+	}*/
+	
+	@Test
+	public void compruebaPerimetro() {
+		assertEquals(24,rectangulo.calculaPerimetro());
+	}
+
+	@Test
+	public void compruebaArea() {
+		assertEquals(36,rectangulo.calculaArea());
+	}
+	
+	@Test
+	public void compruebaDiagonal() {
+		assertEquals(72,rectangulo.calculaDiagonal());
+	}
+	
+	@Test
+	public void compruebaCircuncrita() {
+		assertEquals(36,rectangulo.calculaCircuncrita());
+	}
+	
 }
