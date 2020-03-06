79795e25977858a809d821b5526d7cbd3df48528
DAM_1 <DAM_1@PB06>
2020-03-06 20:55:00 +0100

    Numero 1

--- src/test/java/liceosorolla/NumeroTest.java
+++ src/test/java/liceosorolla/NumeroTest.java
@@ -1,5 +1,23 @@
 package test.java.liceosorolla;
 
-public class NumeroTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import main.java.liceosorolla.Numero;
+
+public class NumeroTest {
+	
+	private Numero numero;
+	
+	@Before
+	public void before() {
+		numero = new Numero(12,6);
+	}
+	
+	@Test
+	public void comprobaRectangulo() {
+		assertEquals(false, numero.getNumero1());
+	}
 }
